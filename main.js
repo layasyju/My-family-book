365b2967c20c23b5c471fa767a761eaedcd7a639
layasyju <75535822+layasyju@users.noreply.github.com>
2021-04-18 20:21:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var reasons=[
+    "Father",
+    "Mother",
+    "Grandad",
+    "Grandmom",
+    "Sister",
+    "Coco",
+    "Polo",
+    ];
+    var images=[
+    "x.png",
+    "mother.png",
+    "grandfather.png",
+    "grandmom.png",
+    "sister.jpg",
+    "puppu.png",
+    "download.png"
+    ];
+    
+    var i=0;
+    function nextslide(){
+    document.getElementById("p3").innerHTML=reasons[i];
+    document.getElementById("i1").src=images[i];
+    i++;
+    }
